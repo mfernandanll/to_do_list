d8e1e540dbd34df24a585b4d804ebde8a23aaabc
Fernanda Loureiro <m.fernanda.nll@gmail.com>
2022-10-31 17:07:56 -0400

    Creating src directory and adding Home screen

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,10 @@
+import { StatusBar, Text, View } from 'react-native';
+import { styles } from './style'
+
+export function Home() {
+    return (
+        <View style={styles.container}>
+            <Text>Home Screen</Text>
+        </View>
+    );
+}
