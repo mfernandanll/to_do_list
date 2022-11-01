b4b8da69be1d89218f62e03b48f609f43ff35d0c
Fernanda Loureiro <m.fernanda.nll@gmail.com>
2022-10-31 21:19:32 -0400

    Creating initial layout for Home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,49 @@
-import { StatusBar, Text, View } from 'react-native';
+import { Image, Text, View, TextInput, TouchableOpacity, FlatList } from 'react-native';
 import { styles } from './style'
 
 export function Home() {
     return (
         <View style={styles.container}>
-            <Text>Home Screen</Text>
+            <Image style={styles.logo}
+                source={require('../../../assets/Logo.png')} />
+            <View style={styles.form}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Adicione uma nova tarefa"
+                    placeholderTextColor="#6B6B6B"
+                    keyboardType="default"
+                />
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>
+                        ⊕
+                    </Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.subtittles}>
+                <View style={styles.counterView}>
+                    <Text style={styles.createdText}>
+                        Criadas
+                    </Text>
+                    <TextInput
+                        style={styles.counter}
+                        value='0'
+                        defaultValue='0'
+                        editable={false}
+                    />
+                </View>
+
+                <View style={styles.counterView}>
+                    <Text style={styles.doneText}>
+                        Concluídas
+                    </Text>
+                    <TextInput
+                        style={styles.counter}
+                        value='0'
+                        defaultValue='0'
+                        editable={false}
+                    />
+                </View>
+            </View>
         </View>
     );
 }
--- src/screens/Home/style.ts
+++ src/screens/Home/style.ts
@@ -3,8 +3,69 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        backgroundColor: '#131016',
+        padding: 10,
+        alignItems: "center"
+    },
+    logo: {
+        marginTop: 70,
+    },
+    input: {
+        backgroundColor: '#1F1E25',
+        borderRadius: 5,
+        color: '#FFF',
+        padding: 16,
+        fontSize: 16,
+        flex: 1, 
+        marginRight: 12
+    },
+    buttonText: {
+        color: '#FFF',
+        fontSize: 24
+    },
+    button: {
+        width: 56,
+        height: 56,
+        borderRadius: 5,
+        backgroundColor: '#1E6F9F',
+        alignItems: 'center', 
+        justifyContent: 'center'
+    },
+    form: {
+        width: '100%', 
+        flexDirection: 'row', 
+        marginTop: 36,
+        marginBottom: 10
+    },
+    subtittles: {
+        width: '100%',
+        marginTop: 20,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    createdText: {
+        fontWeight: 'bold',
+        color:'#1E6F9F',
+    }, 
+    doneText: {
+        fontWeight: 'bold',
+        color:'#8284FA',
+    },
+    counter: {
+        width: 25,
+        paddingTop: 2,
+        paddingBottom: 2,
+        fontSize: 12,
+        backgroundColor: '#333',
+        color: '#fff',
+        borderRadius: 10,
+        marginLeft: 10,
+        textAlign: 'center'
+        
+    },
+    counterView: {
+        height: 19,
+        flexDirection: 'row',
+        alignItems: 'flex-end'
     }
 })
